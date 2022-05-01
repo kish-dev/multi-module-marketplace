f1e9cde9d640368892a15d03fb9378b79363e78c
diskhokov <diskhokov@ozon.ru>
2022-05-01 15:47:15 +0300

    - add docs for ServiceLocator

--- app/src/main/java/ru/ozon/route256/workshop1/sources/ServiceLocator.kt
+++ app/src/main/java/ru/ozon/route256/workshop1/sources/ServiceLocator.kt
@@ -5,6 +5,9 @@ import ru.ozon.route256.workshop1.data.repositories.MockProductsRepository
 import ru.ozon.route256.workshop1.domain.interactors.ProductsInteractor
 import ru.ozon.route256.workshop1.domain.interactors.ProductsInteractorImpl
 
+ // Подробнее можете почитать [тут](http://sergeyteplyakov.blogspot.com/2013/03/di-service-locator.html),
+ // [тут](https://habr.com/ru/post/465395/) и [тут](https://javatutor.net/articles/j2ee-pattern-service-locator).
+
 class ServiceLocator {
     val productsInteractor: ProductsInteractor by lazy {
         ProductsInteractorImpl(
