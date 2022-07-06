495f6066477ca889bb0423d40c96bec498234398
Anton Gulyaev <kishmyak2001@gmail.com>
2022-07-07 01:51:46 +0300

    some additions

--- core-utils/src/main/java/com/software/core_utils/presentation/view_holders/ProductImageViewHolder.kt
+++ core-utils/src/main/java/com/software/core_utils/presentation/view_holders/ProductImageViewHolder.kt
@@ -1,10 +1,8 @@
 package com.software.core_utils.presentation.view_holders
 
-import android.graphics.drawable.Drawable
 import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.RequestBuilder
 import com.software.core_utils.R
 import com.software.core_utils.presentation.common.setImageFromUrl
 
