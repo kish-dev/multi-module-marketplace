a1bc01b7eaa02fdd153c5e535c42c39a3dec7ee3
Anton Gulyaev <kishmyak2001@gmail.com>
2022-07-07 15:06:52 +0300

    deleted TitleProductVO.kt

--- feature-products-impl/src/main/java/com/software/feature_products_impl/presentation/view_objects/TitleProductVO.kt
+++
@@ -1,7 +0,0 @@
-package com.software.feature_products_impl.presentation.view_objects
-
-//
-//data class TitleProductVO(
-//    val headerText: String,
-//    override val viewType: Int = ViewTypes.TITLE,
-//) : BaseProductsTitleModel()
