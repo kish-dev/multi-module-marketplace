e5436347e01e4a1479f0df1b2b994577a8c1d520
Anton Gulyaev <kishmyak@mts.ru>
2024-11-14 14:06:08 +0300

    added backend error

--- core-network-api/src/main/java/com/software/feature_api/ProductsApi.kt
+++ core-network-api/src/main/java/com/software/feature_api/ProductsApi.kt
@@ -6,10 +6,10 @@ import com.software.feature_api.wrappers.ServerResponse
 import retrofit2.http.GET
 
 interface ProductsApi {
-
-    @GET("de9e8c2d-c4cb-458e-83a9-4ddfea568474")
+//https://run.mocky.io/v3/47d95b4a-2c03-45f2-bf21-9e277aec7691
+    @GET("47d95b4a-2c03-45f2-bf21-9e277aec7691")
     suspend fun getProductsInList(): ServerResponse<List<ProductInListDTO>>
 
-    @GET("de9e8c2d-c4cb-458e-83a9-4ddfea568474")
+    @GET("47d95b4a-2c03-45f2-bf21-9e277aec7691")
     suspend fun getProducts(): ServerResponse<List<ProductInListDTO>>
 }
