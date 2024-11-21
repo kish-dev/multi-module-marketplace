77e1f4e2e6ed273565a2f132ab01234d91a36e93
Anton Gulyaev <kishmyak@mts.ru>
2024-11-21 18:27:58 +0300

    some adds

--- core-network-api/src/main/java/com/software/feature_api/ProductsApi.kt
+++ core-network-api/src/main/java/com/software/feature_api/ProductsApi.kt
@@ -6,10 +6,9 @@ import com.software.feature_api.wrappers.ServerResponse
 import retrofit2.http.GET
 
 interface ProductsApi {
-//https://run.mocky.io/v3/47d95b4a-2c03-45f2-bf21-9e277aec7691
-    @GET("47d95b4a-2c03-45f2-bf21-9e277aec7691")
+    @GET("cdcb0ba4-d4f3-47a6-86ea-2f2a20c92030")
     suspend fun getProductsInList(): ServerResponse<List<ProductInListDTO>>
 
-    @GET("47d95b4a-2c03-45f2-bf21-9e277aec7691")
+    @GET("cdcb0ba4-d4f3-47a6-86ea-2f2a20c92030")
     suspend fun getProducts(): ServerResponse<List<ProductInListDTO>>
 }
